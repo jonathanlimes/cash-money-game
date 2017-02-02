7599bcc18642ad78bc13083647fd38cb9c98b22e
Jonathan Lim <jonathanlimes@gmail.com>
2017-02-02 15:21:56 +0800

    fixed negative cash, prevented gameover

--- js/script.js
+++ js/script.js
@@ -59,29 +59,25 @@ function cashMoneyGame () {
   }
   firecrackerImg.src = 'images/firecrackerBig.png'
 
-  // game objs
-  var player = {
+  // set vars for game objs
+  var playerOne = {
     speed: 410, // px per sec
     x: 32,
     y: 32
   }
-
   var playerTwo = {
     speed: 410,
     x: canvas.width - 32,
     y: canvas.height - 32
   }
-
   var orange = {
     x: 0,
     y: 0
   }
-
   var firecracker = {
     x: 0,
     y: 0
   }
-
   var angpow = {
     x: 0,
     y: 0
@@ -91,28 +87,17 @@ function cashMoneyGame () {
   var moneyP1 = 0
   var moneyP2 = 0
 
-  // set vars and functions for timer bar
-  var timerBar = $('#timer-bar'),
-    max = timerBar.attr('max'),
-    value = timerBar.val(),
-    time = (1000 / max) * 30
-
-  function timer () {
-    value += 1
-    addValue = timerBar.val(value)
-    if (value == max) {
-      console.log('timer has run out')
-      clearInterval(runTimer)
-      gameOver()
-    }
-  }
-
-  var runTimer = setInterval(function () {
-    timer()
-  }, time)
-
-  // declare keysDown object and set keydown/keyup functions
+  // vars for timers, 'then', banner, keysDown
+  var timerBar = $('#timer-bar')
+  var max = timerBar.attr('max')
+  var value = timerBar.val()
+  var time = (1000 / max) * 30
+  var runTimer = setInterval(function () { timer() }, time)
+  var then = Date.now()
+  var banner = document.querySelector('#banner')
   var keysDown = {}
+
+  // set keydown/keyup functions
   window.addEventListener('keydown', function (e) {
     keysDown[e.keyCode] = true
   }, false)
@@ -126,12 +111,10 @@ function cashMoneyGame () {
     orange.x = 32 + (Math.random() * (canvas.width - 96))
     orange.y = 32 + (Math.random() * (canvas.height - 96))
   }
-
   function respawnFirecracker () {
     firecracker.x = 100 + (Math.random() * 400)
     firecracker.y = 100 + (Math.random() * 400)
   }
-
   function respawnAngpow () {
     angpow.x = 32 + (Math.random() * (canvas.width - 96))
     angpow.y = 32 + (Math.random() * (canvas.height - 96))
@@ -141,29 +124,16 @@ function cashMoneyGame () {
   function updateGameObjs (modifier) {
     // for movement of players
     if (87 in keysDown) {
-      player.y -= player.speed * modifier
+      playerOne.y -= playerOne.speed * modifier
     }
     if (83 in keysDown) {
-      player.y += player.speed * modifier
+      playerOne.y += playerOne.speed * modifier
     }
     if (65 in keysDown) {
-      player.x -= player.speed * modifier
+      playerOne.x -= playerOne.speed * modifier
     }
     if (68 in keysDown) {
-      player.x += player.speed * modifier
-    }
-    // move within bounds
-    if (player.x < canvas.lBound) {
-      player.x = canvas.lBound
-    }
-    if (player.x > canvas.rBound) {
-      player.x = canvas.rBound
-    }
-    if (player.y < canvas.tBound) {
-      player.y = canvas.tBound
-    }
-    if (player.y > canvas.bBound) {
-      player.y = canvas.bBound
+      playerOne.x += playerOne.speed * modifier
     }
     if (38 in keysDown) {
       playerTwo.y -= playerTwo.speed * modifier
@@ -178,7 +148,19 @@ function cashMoneyGame () {
       playerTwo.x += playerTwo.speed * modifier
     }
 
-    // to prevent players from moving beyond game area borders
+    // to stop movement beyond game boundaries
+    if (playerOne.x < canvas.lBound) {
+      playerOne.x = canvas.lBound
+    }
+    if (playerOne.x > canvas.rBound) {
+      playerOne.x = canvas.rBound
+    }
+    if (playerOne.y < canvas.tBound) {
+      playerOne.y = canvas.tBound
+    }
+    if (playerOne.y > canvas.bBound) {
+      playerOne.y = canvas.bBound
+    }
     if (playerTwo.x < canvas.lBound) {
       playerTwo.x = canvas.lBound
     }
@@ -192,18 +174,19 @@ function cashMoneyGame () {
       playerTwo.y = canvas.bBound
     }
 
-    // collisions with sprite items
+    // collision events
     if (
-      player.x <= (angpow.x + 32)
-      && angpow.x <= (player.x + 32)
-      && player.y <= (angpow.y + 32)
-      && angpow.y <= (player.y + 32)
+      playerOne.x <= (angpow.x + 32)
+      && angpow.x <= (playerOne.x + 32)
+      && playerOne.y <= (angpow.y + 32)
+      && angpow.y <= (playerOne.y + 32)
     ) {
       $(money).get(0).play()
       moneyP1 += 2
-      player.speed -= 50
+      playerOne.speed -= 50
       respawnAngpow()
-    } else if (
+    }
+    if (
       playerTwo.x <= (angpow.x + 32)
       && angpow.x <= (playerTwo.x + 32)
       && playerTwo.y <= (angpow.y + 32)
@@ -215,13 +198,13 @@ function cashMoneyGame () {
       respawnAngpow()
     }
     if (
-      player.x <= (orange.x + 32)
-      && orange.x <= (player.x + 32)
-      && player.y <= (orange.y + 32)
-      && orange.y <= (player.y + 32)
+      playerOne.x <= (orange.x + 32)
+      && orange.x <= (playerOne.x + 32)
+      && playerOne.y <= (orange.y + 32)
+      && orange.y <= (playerOne.y + 32)
     ) {
       $(powerup).get(0).play()
-      player.speed += 100
+      playerOne.speed += 100
       respawnOrange()
     }
     if (
@@ -235,15 +218,20 @@ function cashMoneyGame () {
       respawnOrange()
     }
     if (
-      player.x <= (firecracker.x + 85)
-      && firecracker.x <= (player.x + 32)
-      && player.y <= (firecracker.y + 85)
-      && firecracker.y <= (player.y + 32)
+      playerOne.x <= (firecracker.x + 85)
+      && firecracker.x <= (playerOne.x + 32)
+      && playerOne.y <= (firecracker.y + 85)
+      && firecracker.y <= (playerOne.y + 32)
     ) {
-      player.x = 32
-      player.y = 32
+      playerOne.x = 32
+      playerOne.y = 32
       $(explosion).get(0).play()
-      moneyP1 -= 3
+      if (moneyP1 >= 0) {
+        moneyP1 -= 3
+      }
+      if (moneyP1 < 0) {
+        moneyP1 = 0
+      }
       respawnFirecracker()
     }
     if (
@@ -255,7 +243,12 @@ function cashMoneyGame () {
       playerTwo.x = canvas.width - 32
       playerTwo.y = canvas.height - 32
       $(explosion).get(0).play()
-      moneyP2 -= 3
+      if (moneyP2 >= 0) {
+        moneyP2 -= 3
+      }
+      if (moneyP2 < 0) {
+        moneyP2 = 0
+      }
       respawnFirecracker()
     }
   }
@@ -266,7 +259,7 @@ function cashMoneyGame () {
       ctx.drawImage(backgroundImg, 0, 0)
     }
     if (isPlayerReady) {
-      ctx.drawImage(playerImg, player.x, player.y)
+      ctx.drawImage(playerImg, playerOne.x, playerOne.y)
     }
     if (isPlayerTwoReady) {
       ctx.drawImage(playerTwoImg, playerTwo.x, playerTwo.y)
@@ -289,17 +282,31 @@ function cashMoneyGame () {
   }
 
   // change content of word banner when timer runs out
-  var banner = document.querySelector('#banner')
   function gameOver () {
     if (moneyP1 > moneyP2) {
       banner.textContent = 'P1 WINS WITH $' + moneyP1 + '!'
       alert('Player 1 wins!')
+      $('#banner').css('font-size', '70px')
     } else if (moneyP2 > moneyP1) {
       banner.textContent = 'P2 WINS WITH $' + moneyP2 + '!'
       alert('Player 2 wins!')
+      $('#banner').css('font-size', '70px')
     } else {
       banner.textContent = 'DRAW!'
       alert("Draw! Hit 'Reset Game' to play again!")
+      $('#banner').css('font-size', '100px')
+    }
+    $('canvas').remove()
+  }
+
+  // run timer
+  function timer () {
+    value += 1
+    addValue = timerBar.val(value)
+    if (value == max) {
+      console.log('timer has run out')
+      clearInterval(runTimer)
+      gameOver()
     }
   }
 
@@ -307,14 +314,6 @@ function cashMoneyGame () {
   var resetButton = document.querySelector('button')
   resetButton.addEventListener('click', function () {
     location.reload()
-    // angpowsP1 = 0
-    // angpowsP2 = 0
-    // player.x = (canvas.width/3)-32
-    // player.y = canvas.height/2
-    // playerTwo.x = (canvas.width/3)*2
-    // playerTwo.y = canvas.height/2
-    // banner.textContent = "CASH MONEY"
-    // timerBar.reset()
   })
 
   // main function to initiate and loop the whole game
@@ -332,7 +331,6 @@ function cashMoneyGame () {
   requestAnimationFrame = window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame || window.mozRequestAnimationFrame
 
   // run all functions
-  var then = Date.now()
   respawnAngpow()
   respawnOrange()
   respawnFirecracker()
